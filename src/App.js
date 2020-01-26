5c0cbf7ff920c5747dfc6db51ddf36713888f0f1
Mungert69 <43328293+Mungert69@users.noreply.github.com>
2020-01-26 21:38:06 +0000

    added bootstrap style to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { CustomTable } from './Components/table';
 import TestApi from './Components/TestApi';
 import moment from "moment";
 import { Row, Col, Container, Button } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 

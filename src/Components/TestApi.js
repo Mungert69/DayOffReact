559c1b494b28f7387e69c5486002de40ef6a19f2
Mahadeva <43328293+Mungert69@users.noreply.github.com>
2020-09-29 17:50:54 +0200

    changed to http from https

--- src/Components/TestApi.js
+++ src/Components/TestApi.js
@@ -8,7 +8,7 @@ import UserContractInfo from './UserContractInfo';
 import Spinny from './Spinny.gif'; // with import
 
 
-const apiBaseUrl = 'https://heartiq.hopto.org:65125/';
+const apiBaseUrl = 'http://heartiq.hopto.org:65125/';
 
 const fontStyle = { color: 'green' };
 const customStyles = {
